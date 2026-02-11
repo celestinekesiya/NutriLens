c82aa9e0a9f5bff05ea27cf2756e5662cdf61622
santhosh-005 <santhoshdon000555@gmail.com>
2026-02-11 18:05:05 +0530

    feat: deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,34 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name!.split('.')
+          const extType = info[info.length - 1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            return `assets/img/[name].[hash][extname]`
+          }
+          if (/css/i.test(extType)) {
+            return `assets/css/[name].[hash][extname]`
+          }
+          return `assets/[name].[hash][extname]`
+        },
+      },
+    },
+  },
+  server: {
+    headers: {
+      'Content-Type': 'text/css; charset=utf-8',
+    },
+    middlewareMode: false,
+  },
+  preview: {
+    headers: {
+      'Content-Type': 'text/css; charset=utf-8',
+    },
+  },
 })
